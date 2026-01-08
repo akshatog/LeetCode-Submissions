6cc091913e2fd49648d2720b062c541ed6800864
Akshat Agarwal <akshatsanghi20@gmail.com>
2026-01-08 17:03:06 +0000

    Sync LeetCode submission - Reverse Integer (cpp)

--- my-folder/problems/reverse_integer/solution.cpp
+++ my-folder/problems/reverse_integer/solution.cpp
@@ -1,23 +1,35 @@
 class Solution {
 public:
     int reverse(int x) {
-        int temp = x;
-        int temp2 = x;
+        
+        // Brute force
+        // int temp = x;
+        // int temp2 = x;
+        // long long rev = 0;
+        // int rem;
+        // int count = 0;
+        // while(temp != 0){
+        //     temp /= 10;
+        //     count++;
+        // }
+        // for(int i = count-1;i>=0;i--){
+        //     rem = temp2 % 10;
+        //     temp2 = temp2 / 10;
+        //     rev = rev + rem*(long long)pow(10,i); 
+        // }
+        // if(rev >= INT_MAX || rev<= INT_MIN){
+        //     return 0;
+        // }
+        // else return rev;
+
+        // //Optimized
         long long rev = 0;
-        int rem;
-        int count = 0;
-        while(temp != 0){
-            temp /= 10;
-            count++;
+        while(x != 0){
+            int rem = x%10;
+            x /= 10;
+            rev = rev*10 + rem;
         }
-        for(int i = count-1;i>=0;i--){
-            rem = temp2 % 10;
-            temp2 = temp2 / 10;
-            rev = rev + rem*(long long)pow(10,i); 
-        }
-        if(rev >= INT_MAX || rev<= INT_MIN){
-            return 0;
-        }
+        if(rev>INT_MAX || rev<INT_MIN) return 0;
         else return rev;
-    }
+    } 
 };
