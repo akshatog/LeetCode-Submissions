93b0512b238bb7329f306ee24bb3d2df3985fe84
Akshat Agarwal <akshatsanghi20@gmail.com>
2026-06-27 09:58:29 +0000

    Sync LeetCode submission - Permutations II (python)

---
+++ my-folder/problems/permutations_ii/solution.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+
+        nums.sort()
+
+        res = []
+        path = []
+        used = [False] * len(nums)
+
+        def backtrack():
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+
+            for i in range(len(nums)):
+
+                if used[i]:
+                    continue
+
+                # Skip duplicates
+                if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                    continue
+
+                used[i] = True
+                path.append(nums[i])
+
+                backtrack()
+
+                path.pop()
+                used[i] = False
+
+        backtrack()
+        return res
