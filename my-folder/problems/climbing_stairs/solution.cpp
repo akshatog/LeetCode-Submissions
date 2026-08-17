2da66457d65e93ce97d33144c38d07fd99e1a62c
Akshat Agarwal <akshatsanghi20@gmail.com>
2026-08-17 10:15:25 +0000

    Sync LeetCode submission - Climbing Stairs (cpp)

---
+++ my-folder/problems/climbing_stairs/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int climb(int n, vector<int> & memo){
+        if(n<=2)
+            return n;
+        
+        if(memo[n] != -1)
+            return memo[n];
+        
+        memo[n] = climb(n-1,memo) + climb(n-2,memo);
+
+        return memo[n];
+    }
+
+    int climbStairs(int n) {
+       vector<int> memo(n+1,-1);
+       return climb(n,memo);
+    }
+};
