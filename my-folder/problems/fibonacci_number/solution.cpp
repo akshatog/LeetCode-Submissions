a86ea233e4d3ac819ec69bcfa86c8388bf5a9533
Akshat Agarwal <akshatsanghi20@gmail.com>
2026-08-18 04:01:23 +0000

    Sync LeetCode submission - Fibonacci Number (cpp)

--- my-folder/problems/fibonacci_number/solution.cpp
+++ my-folder/problems/fibonacci_number/solution.cpp
@@ -1,25 +1,14 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n==0){
-            return 0;
-        }
-        else if(n==1){
-            return 1;
-        }
-        // else{
-        //     return fib(n-2) + fib(n-1);
-        // }
-        else {
-// int next_num; - instead of using the 3rd variable ere i can simply return b as i am putting it's value in b itself. so i can declare it inside the loop and reduce my space complexity.
-            int a = 0, b = 1;
-            for(int i = 1;i<n;i++){
-                int next_num = a + b;
-                a = b;
-                b = next_num;
-            }
-            return b;
-        }
+        if(n<2) return n;
 
+        int prev1 = 1, prev2=0;
+        for(int i=2;i<=n;i++){
+            int curr = prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
+      }
+      return prev1;
     }
 };
